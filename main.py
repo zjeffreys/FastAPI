8da954070bf18ebdef73a8196f17924a1f4c916a
zjeffreys <zjeffreys@seattlu.edu>
2023-10-31 23:48:00 -0700

    fixed requirments

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+import openai
 
 app = FastAPI()
 
-@app.get("/")
-async def root():
-    return {"greeting": "Hello, World!", "message": "Welcome to FastAPI!"}
+class ChatMessage(BaseModel):
+    message: str
+
+@app.post("/getChatResponse")
+def get_chat_response(message: ChatMessage):
+    # Send the user's message to GPT-3.5 and get a response
+    response = openai.Completion.create(
+        engine="text-davinci-002",
+        prompt=message.message,
+        max_tokens=50,  # Adjust as needed
+    )
+    
+    return {"response": response.choices[0].text.strip()}
